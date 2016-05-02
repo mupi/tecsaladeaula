07fb1276cd7c8bcba53a89134b196c26b25c9faa
Ana Rute <anarutecvm@gmail.com>
2016-05-02 16:12:59 -0300

    lesson: add next lesson button

---
+++ core/templatetags/return_next_lesson.py
@@ -0,0 +1,20 @@
+from django import template
+
+register = template.Library()
+
+@register.filter
+def return_next_slug(l, i):
+    try:
+        lesson = l[i+1]
+        return lesson.slug
+    except:
+        return None
+
+
+@register.filter
+def return_next_name(l, i):
+    try:
+        lesson = l[i+1]
+        return lesson.name
+    except:
+        return None
