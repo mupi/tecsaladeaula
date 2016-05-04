86cea8a7f42e3266d58024d3714b37bdf36c18fa
Ana Rute <anarutecvm@gmail.com>
2016-05-04 15:51:52 -0300

    add previous lesson button

--- core/templatetags/return_next_and_prev_lesson.py
+++ core/templatetags/return_next_and_prev_lesson.py
@@ -19,3 +19,21 @@ def return_next_name(l, i):
         return lesson.name
     except:
         return None
+
+
+@register.filter
+def return_prev_slug(l, i):
+    try:
+        lesson = l[i - 1]
+        return lesson.slug
+    except:
+        return None
+
+
+@register.filter
+def return_prev_name(l, i):
+    try:
+        lesson = l[i - 1]
+        return lesson.name
+    except:
+        return None
