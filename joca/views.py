48a2eac27e70274a08319af4adb1ba232942623e
Allan Nozomu Fukasawa <allannozomu@gmail.com>
2017-09-21 12:34:07 -0300

    Change the joca singup and login page. Redirect to the course if login/signup came from joca's page

--- joca/views.py
+++ joca/views.py
@@ -15,13 +15,24 @@ class JocaSignupView(SignupView):
     redirect_field_name = 'next'
     view_name = 'joca_signup'
 
+    def get_context_data(self, **kwargs):
+        context = super(JocaSignupView, self).get_context_data(**kwargs)
+
+        course = Course.objects.get(id=11)
+        context['next'] = reverse_lazy('course_intro', args=[course.slug])
+
+        return context
+
     def get_success_url(self):
         try:
-            user = TimtecUser.objects.get(email = self.request.POST['email'])
+            if ('email' in self.request.POST):
+                user = TimtecUser.objects.get(email = self.request.POST['email'])
+            else:
+                user = self.request.user
 
             course = Course.objects.get(id=11)
-            course.enroll_student(user)
-            if course.has_started and course.first_lesson():
-                return reverse_lazy('course_intro', args=[course.slug])
+            if not course.is_enrolled(user):
+                course.enroll_student(user)
+            return reverse_lazy('course_intro', args=[course.slug])
         except:
             return reverse_lazy('courses')
