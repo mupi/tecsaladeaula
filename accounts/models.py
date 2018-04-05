e26e2ed12b0d4844ab7ef3b92c52be1de1fefccb
Allan Nozomu Fukasawa <allannozomu@gmail.com>
2018-04-05 09:30:11 -0300

    Add occupations to User model

--- accounts/models.py
+++ accounts/models.py
@@ -108,11 +108,13 @@ class City (models.Model):
 
 #populated by fixture
 class Discipline (models.Model):
-    name = models.CharField(max_length=200, unique=True,primary_key=True)
+    name = models.CharField(max_length=100, unique=True,primary_key=True)
 
     def __str__(self):
         return self.name
 
+class Occupation(models.Model):
+    name = models.CharField(max_length=100)
 
 #populated by fixture
 class EducationDegree (models.Model):
@@ -136,8 +138,7 @@ class TimtecUser(AbstractTimtecUser):
     school = models.ManyToManyField(School, blank=True, null=True)
     disciplines = models.ManyToManyField(Discipline, blank=True, null=True)
     city = models.ForeignKey(City, on_delete=models.CASCADE, null=True, blank=True)
-
-    job_title = models.CharField(max_length=70, blank=True, null=True)
+    occupations = models.ManyToManyField(Occupation, blank=True, null=True)
 
     class Meta(AbstractTimtecUser.Meta):
         swappable = 'AUTH_USER_MODEL'
