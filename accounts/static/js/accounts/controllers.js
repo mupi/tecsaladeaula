ac142b93fcdf89bf86f43132d8e7461bf4561ee4
Allan Nozomu Fukasawa <allannozomu@gmail.com>
2018-04-18 11:25:46 -0300

    Code refactor, reset form bug fix

--- accounts/static/js/accounts/controllers.js
+++ accounts/static/js/accounts/controllers.js
@@ -7,32 +7,34 @@
     app.controller('ProfileCtrl', ['$scope', '$rootScope', '$location', '$sce', '$window', 'Cities', 'States', 'Occupations', 'Disciplines', 'EducationDegrees', 'UserSchools', 'Profile',
             function ($scope, $rootScope, $location, $sce, $window, Cities, States, Occupations, Disciplines, EducationDegrees, UserSchools, Profile) {
                 
-                $scope.occupations = {};
-                $scope.disciplines = {};
-                $scope.education_degrees = {};
+                $scope.form = {};
+                $scope.form.occupations = {};
+                $scope.form.disciplines = {};
+                $scope.form.disciplines.selected = [];
+                $scope.form.education_degrees = {};
 
                 $scope.avaiable_occupations = Occupations.query();
                 $scope.avaiable_disciplines = Disciplines.query();
                 $scope.avaiable_education_degrees = EducationDegrees.query();
 
-                States.query(function(states){
-                    $scope.list_states = states;
-                    $scope.filters_selected_uf = states[0];
+                States.query(function(ufs){
+                    $scope.list_ufs = ufs;
+                    $scope.form.uf = ufs[0];
                 });
 
                 var load_profile = function(){
                     Profile.get(function(profile){
                         if (profile.city != null){
-                            $scope.selected_city_id = profile.city.id;
-                            $scope.filters_selected_uf = profile.city.uf;
+                            $scope.form.city_id = profile.city.id;
+                            $scope.form.uf = profile.city.uf;
 
-                            Cities.query({uf : $scope.filters_selected_uf}, function(cities){
+                            Cities.query({uf : $scope.form.uf}, function(cities){
                                 $scope.list_cities = cities;
                             });
                         }
-                        $scope.occupations.selected = profile.occupations;
-                        $scope.disciplines.selected = profile.disciplines;
-                        $scope.education_degrees.selected = profile.education_degrees;
+                        $scope.form.occupations.selected = profile.occupations;
+                        $scope.form.disciplines.selected = profile.disciplines;
+                        $scope.form.education_degrees.selected = profile.education_degrees;
 
                         $scope.school_infos = profile.schools;
                     })
@@ -40,22 +42,20 @@
                 load_profile();
 
                 $scope.filter_cities = function(){
-                    Cities.query({uf : $scope.filters_selected_uf}, function(cities){
+                    Cities.query({uf : $scope.form.id}, function(cities){
                         $scope.list_cities = cities;
-                        $scope.selected_city_id = null;
+                        $scope.form.city_id = cities[0].id;
                     });
                 }
 
                 $scope.add_discipline = function(){
-                    if ($scope.other_discipline == '')
+                    if ($scope.form.other_discipline.trim() == '')
                         return;
-                    if ($scope.disciplines.selected == null){
-                        $scope.disciplines.selected = [{name: $scope.other_discipline}];
-                    } else {
-                        $scope.disciplines.selected.push({name: $scope.other_discipline});
-                    }
+                    else 
+                        $scope.form.disciplines.selected.push({name: $scope.form.other_discipline});
                     
-                    $scope.other_discipline = null;
+                    
+                    $scope.form.other_discipline = null;
                 }
 
                 $scope.school_form_modal = function(id){
@@ -79,30 +79,31 @@
         app.controller('SchoolCtrl', ['$scope', '$rootScope', '$http', '$location', '$sce', '$window', 'Cities', 'States', 'EducationLevels', 'UserSchools', 'SchoolTypes',
         function ($scope, $rootScope, $http, $location, $sce, $window, Cities, States, EducationLevels, UserSchools, SchoolTypes) {
             
-            $scope.has_errors = false;
             $scope.form = {};
+            $scope.form.has_errors = false;
             
-            $scope.education_levels = {};
-            $scope.list_states = [];
-            $scope.education_levels.selected = [];
+            $scope.form.education_levels = {};
+            $scope.form.education_levels.selected = [];
+
+            $scope.list_ufs = [];
             $scope.avaiable_education_levels = EducationLevels.query();
 
-            SchoolTypes.query(function(schooltypes){
-                $scope.schooltypes = schooltypes;
-                $scope.form.school_type = schooltypes[0].value;
+            SchoolTypes.query(function(list_schooltypes){
+                $scope.list_schooltypes = list_schooltypes;
+                $scope.form.school_type = list_schooltypes[0].value;
             });
 
-            States.query(function(states){
-                $scope.list_states = states;
-                $scope.filters_selected_uf = states[0];
+            States.query(function(ufs){
+                $scope.list_ufs = ufs;
+                $scope.form.uf = ufs[0];
                 $scope.filter_cities();
             });
             
 
             $scope.filter_cities = function(){
-                Cities.query({uf : $scope.filters_selected_uf}, function(cities){
+                Cities.query({uf : $scope.form.uf}, function(cities){
                     $scope.list_cities = cities;
-                    $scope.form.city = cities[0].id;
+                    $scope.form.city_id = cities[0].id;
                 });
             }
 
@@ -110,7 +111,7 @@
                 $("#btn-add-school-table").prop('disabled', true);
 
                 var education_levels = [];
-                $scope.education_levels.selected.forEach(function(el){
+                $scope.form.education_levels.selected.forEach(function(el){
                     education_levels.push(el.slug);
                 });
 
@@ -126,11 +127,10 @@
                     user_school.id = $scope.form.id;
                 user_school.school.name = $scope.form.name;
                 user_school.school.school_type = $scope.form.school_type;
-                user_school.school.city = $scope.form.city;
+                user_school.school.city = $scope.form.city_id;
                 user_school.education_levels = education_levels;
 
                 if ($scope.form.id > 0){
-                    console.log(user_school);
                     user_school.$update()
                     .then(function(res) {
                         $rootScope.$broadcast('reload_profile');
@@ -138,7 +138,7 @@
                         $
                     })
                     .catch(function(req){
-                        $scope.has_errors = true;
+                        $scope.form.has_errors = true;
                     })
                 } else {
                     user_school.$save()
@@ -148,7 +148,7 @@
                         $
                     })
                     .catch(function(req){
-                        $scope.has_errors = true;
+                        $scope.form.has_errors = true;
                     })
                 }
                 $("#btn-add-school-table").prop('disabled', false);
@@ -159,29 +159,26 @@
                 $scope.form.id = id;
                 if (id > 0){
                     UserSchools.get({id: id}, function(user_school){
-                        $scope.filters_selected_uf = user_school.school.city.uf;
-                        Cities.query({uf : $scope.filters_selected_uf}, function(cities){
+                        $scope.form.uf = user_school.school.city.uf;
+                        Cities.query({uf : $scope.form.uf}, function(cities){
                             $scope.list_cities = cities;
-                            $scope.form.city = user_school.school.city.id;
+                            $scope.form.city_id = user_school.school.city.id;
                         });
                         
                         $scope.form.name = user_school.school.name;
                         $scope.form.school_type = user_school.school.school_type;
-                        $scope.education_levels.selected = user_school.education_levels;
-                        console.log(user_school);
+                        $scope.form.education_levels.selected = user_school.education_levels;
                     });
                 } else {
                     $scope.form.name = "";
-                    $scope.has_errors = false;
-                    $scope.form.school_type = $scope.schooltypes[0].value;
-                    $scope.education_levels.selected = [];
-                    $scope.filters_selected_uf = $scope.list_states[0];
-                    $scope.filter_cities = function(){
-                        Cities.query({uf : $scope.filters_selected_uf}, function(cities){
-                            $scope.list_cities = cities;
-                            $scope.form.city = cities[0].id;
-                        });
-                    }
+                    $scope.form.has_errors = false;
+                    $scope.form.school_type = $scope.list_schooltypes[0].value;
+                    $scope.form.education_levels.selected = [];
+                    $scope.form.uf = $scope.list_ufs[0];
+                    Cities.query({uf : $scope.form.uf}, function(cities){
+                        $scope.list_cities = cities;
+                        $scope.form.city_id = cities[0].id;
+                    });
                 }
             }
 
