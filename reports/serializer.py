cb4bf27adb44baa6c07969a177b130371134872b
Allan Nozomu Fukasawa <allannozomu@gmail.com>
2018-04-23 11:55:30 -0300

    Changed serializers, removed unused filters, display the data

--- reports/serializer.py
+++ reports/serializer.py
@@ -2,42 +2,24 @@
 from rest_framework import serializers
 from core.models import CourseStudent, Course
 
+from accounts.serializers import TimtecProfileSerializer
+
 
 class UserCourseStatsSerializer(serializers.ModelSerializer):
 
-    name = serializers.SerializerMethodField('get_full_name')
-    username = serializers.SerializerMethodField('get_username')
-    email = serializers.SerializerMethodField('get_email')
-    user_id = serializers.SerializerMethodField('get_user_id')
+    user = TimtecProfileSerializer(required=False, read_only=True)
     course_progress = serializers.SerializerMethodField('get_user_progress')
-    # forum_questions = serializers.SerializerMethodField('get_forum_questions')
-    # forum_answers = serializers.SerializerMethodField('get_forum_answers')
-#     lessons_stats = LessonUserStatsSerializer(many=True, allow_add_remove=False)
+    last_access = serializers.SerializerMethodField('get_last_access')
 
     class Meta:
         model = CourseStudent
-        fields = ('name', 'username', 'email', 'user_id', 'course_progress',)
-
-    def get_full_name(self, obj):
-        return obj.user.get_full_name()
-
-    def get_username(self, obj):
-        return obj.user.username
-
-    def get_email(self, obj):
-        return obj.user.email
-
-    def get_user_id(self, obj):
-        return obj.user.id
+        fields = ('user', 'course_progress', 'created_at', 'last_access')
 
     def get_user_progress(self, obj):
         return obj.percent_progress()
 
-    # def get_forum_questions(self, obj):
-    #     return obj.forum_questions_by_lesson()
-    #
-    # def get_forum_answers(self, obj):
-    #     return obj.forum_answers_by_lesson()
+    def get_last_access(self, obj):
+        return obj.get_last_access()
 
 
 class LessonUserStats(serializers.ModelSerializer):
