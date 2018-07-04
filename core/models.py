badfbe75779d2d9e4a796956642a632dfea5dd32
Allan Nozomu Fukasawa <allannozomu@gmail.com>
2018-07-04 17:52:12 -0300

    Show 404 page if user is not enrolled and try to see the course

--- core/models.py
+++ core/models.py
@@ -132,16 +132,16 @@ class Course(models.Model):
             self.default_class.students.add(student)
             send_mail('Usuário Cadastrou-se em um Curso', get_template('core/email/email_user_signed_up_course_support.txt').render(Context({'name': student_name, 'course_name': self.name, 'datetime': now.strftime("%d/%m/%Y - %H:%M"), 'email': student.email})), settings.EMAIL_SUPPORT, [settings.EMAIL_SUPPORT])
         if not CourseStudent.objects.filter(course=self, user=student).exists():
-            if self.tuition == 0:
-                send_mail('Inscrição em Curso Mupi', get_template('core/email/email_user_signed_up_free_course.txt').render(Context({'name': student_name, 'course_name': self.name})), settings.DEFAULT_FROM_EMAIL, [student.email])
-                CourseStudent.objects.create(course=self, user=student, status='2')
-            else:
+            if self.tuition != 0 or self.private:
                 CourseStudent.objects.create(course=self, user=student)
                 if self.private:
                     send_mail('Inscrição em Curso Mupi', get_template('core/email/email_user_signed_up_private_course.txt').render(Context({'name': student_name, 'course_name': self.name})), settings.DEFAULT_FROM_EMAIL, [student.email])
                 else:
                     send_mail('Inscrição em Curso Mupi', get_template('core/email/email_user_signed_up_paid_course.txt').render(Context({'name': student_name, 'course_name': self.name, 'course_link': course_link})), settings.DEFAULT_FROM_EMAIL, [student.email])
-
+            else:
+                send_mail('Inscrição em Curso Mupi', get_template('core/email/email_user_signed_up_free_course.txt').render(Context({'name': student_name, 'course_name': self.name})), settings.DEFAULT_FROM_EMAIL, [student.email])
+                CourseStudent.objects.create(course=self, user=student, status='2')
+                
     def is_enrolled(self, user):
         return CourseStudent.objects.filter(course=self, user=user, status=CourseStudent.STATES[1][0]).exists()
 
